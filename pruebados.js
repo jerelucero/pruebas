2c61fb27ab8dc3e7bc6ef4f81210abd89e150e42
jerelucero <ramilucerook@gmail.com>
2025-09-01 10:05:28 -0300

    first commit

---
+++ pruebados.js
@@ -0,0 +1,87 @@
+// const variableuno = 2345345345;
+// const variabledos = 9234234234;
+
+// function sumar() {
+//   let resultado = variableuno + variabledos;
+//   return resultado;
+// }
+
+// function multiplicar () {
+//     let resultado = variableuno * variabledos
+//     return resultado
+// }
+
+// function dividir() {
+//     let resultado = variabledos / variableuno
+//     return resultado
+// }
+
+// console.log(dividir());
+// console.log(multiplicar());
+// console.log(sumar());
+
+// string = "" =>  el string es igual a cadena de texto osea ""
+
+//ETAPA "2"
+// const variableuno = 23123123;
+// const variabledos = 1;
+
+// function sumar() {
+//     if( variabledos > 0){
+//         let resultado = variableuno + variabledos;
+//         return resultado;
+//     }else{
+//         console.log("la variable dos no es mayor que 0")
+//     }
+// }
+
+// function multiplicar () {
+//     let resultado = variableuno * variabledos
+//     return resultado
+// }
+
+// function dividir() {
+//     let resultado = variabledos / variableuno
+//     return resultado
+// }
+
+// const variableuno = 23123123;
+// const variabledos = 1;
+
+// function sumar() {
+//     if( variabledos > 0){
+//         let resultado = variableuno + variabledos;
+//         return resultado;
+//     }else{
+//         console.log("la variable dos no es mayor que 0")
+//     }
+// }
+
+// function multiplicar () {
+//     let resultado = variableuno * variabledos
+//     return resultado
+// }
+
+// function dividir() {
+//     let resultado = variabledos / variableuno
+//     return resultado
+// }
+
+// console.log(dividir());
+// console.log(multiplicar());
+// console.log(sumar());
+
+// let fechaNacimiento = 2009;
+// if (fechaNacimiento <= 2006) {
+//   console.log("felicitaciones es mayor de edad");
+// } else {
+//   console.log("no es mayor de edad");
+// }
+
+
+let variabletres = 88;
+let variablecuatro = 77;
+function sumar (numerouno,numerodos){
+  return numerouno + numerodos
+}
+console.log (sumar(variabletres,variablecuatro))
